d122396b72bde1748b50ceee48fdad001a20f9b6
Maurici Alvarez <maurici.alvarez@gmail.com>
2022-03-24 10:02:39 -0300

    Alura ASP.NET Core REST APIs - primeiro modulo

---
+++ FilmesAPI/FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,90 @@
+﻿using AutoMapper;
+using FilmesAPI.Data;
+using FilmesAPI.Data.DTOs;
+using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+        private FilmeContext _context;
+        private IMapper _mapper;
+
+        public FilmeController(FilmeContext context, IMapper mapper)
+        {
+            this._context = context;
+            this._mapper = mapper;
+        }
+
+        [HttpPost]
+        public IActionResult AdicionaFilme([FromBody] CreateFilmeDTO filmeDTO)
+        {
+            var filme = _mapper.Map<Filme>(filmeDTO);
+
+            _context.Filmes.Add(filme);
+            Console.WriteLine(filme.Titulo);
+            _context.SaveChanges();
+
+            return CreatedAtAction(nameof(RecuperarFilmesPorId), new { ID = filme.ID }, filme);
+        }
+
+        [HttpGet]
+        public IActionResult RecuperarFilmes()
+        {
+            return Ok(_context.Filmes);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult RecuperarFilmesPorId(Guid id)
+        {
+            var filme = _context.Filmes.Where(f => f.ID == id).FirstOrDefault();
+
+            if (filme != null)
+            {
+                var filmeDTO = _mapper.Map<ReadFilmeDTO>(filme);
+                //filmeDTO.DataHoraConsulta = new DateTime().Date;
+                return Ok(filmeDTO);
+            }
+            return NotFound();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult AtualizaFilme(Guid id, [FromBody] UpdateFilmeDTO filmeDTO)
+        {
+            var filme = _context.Filmes.Where(f => f.ID == id).FirstOrDefault();
+
+            if (filme == null)
+            {
+                
+                return NotFound();
+            }
+
+            _mapper.Map(filmeDTO, filme);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeletaFilme(Guid id)
+        {
+            var filme = _context.Filmes.Where(f => f.ID == id).FirstOrDefault();
+
+            if (filme == null)
+            {
+                return NotFound();
+            }
+            _context.Remove(filme);
+            _context.SaveChanges();
+            return NoContent();
+        }
+
+    }
+}
---
+++ FilmesAPI/FilmesAPI/Migrations/20220324011725_CriandoTabelaDeFilme.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FilmesAPI.Migrations
+{
+    public partial class CriandoTabelaDeFilme : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Filmes",
+                columns: table => new
+                {
+                    ID = table.Column<byte[]>(type: "varbinary(16)", nullable: false),
+                    Titulo = table.Column<string>(type: "text", nullable: false),
+                    Diretor = table.Column<string>(type: "text", nullable: false),
+                    Genero = table.Column<string>(type: "varchar(30)", maxLength: 30, nullable: true),
+                    Duracao = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Filmes", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Filmes");
+        }
+    }
+}
